57c8d8ca159338ec4eb4678edd39a58842e44aa7
yujinpan <1192878390@qq.com>
2018-12-24 11:47:45 +0800

    feat: 增加pv共享组件

---
+++ src/components/Pv.vue
@@ -0,0 +1,32 @@
+<!-- IP浏览量共享组件 -->
+<template>
+  <div class="pv">
+    <p v-if="total">第{{total}}位访客～</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { setPv } from '@/api/pv';
+
+@Component
+export default class Pv extends Vue {
+  total: number = 0;
+
+  created() {
+    // pv/set
+    setPv().then((resp: any) => {
+      this.total = resp.total;
+    });
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.pv {
+  font-size: 16px;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+}
+</style>
